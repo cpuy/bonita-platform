84a4f4cde07d4674c129b2c565af67007d7dd075
Colin PUY <colin.puy@bonitasoft.com>
2014-07-07 17:56:49 +0200

    project scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('bonitaPlatform')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.test = "hello world";
+  });
