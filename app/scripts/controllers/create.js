82fcf5ad59455e4d0f76514fb408cc724d715bf0
julienmege <julien.mege@bonitasoft.com>
2014-07-08 16:01:37 +0200

    validation
    Conflicts:
    	app/scripts/controllers/create.js
    	app/views/create.html

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,20 +1,34 @@
 angular.module('bonitaPlatform')
     .controller('CreateCtrl', function ($scope, $http, $location) {
-        $scope.submit = function (tenant) {
-            var state;
-            if (tenant.activated) {
-                state = "ACTIVATED";
-            } else {
-                state = "DEACTIVATED";
+        $scope.showValidationMessages = false;
+
+        $scope.isNotValid = function(field){
+            return $scope.showValidationMessages && field.$invalid;
+        }
+
+        $scope.submit =  function(tenant) {
+           var state;
+           $scope.showValidationMessages = true;
+           $scope.form.$setDirty();
+           if($scope.form.$valid) {
+               $scope.showValidationMessages = false;
+
+               if (tenant.activated) {
+                   state = "ACTIVATED";
+               } else {
+                   state = "DEACTIVATED";
+               }
+
+               $http.post("/bonita/API/platform/tenant", {"name": tenant.name, "username": tenant.username, "password": tenant.password, "description": tenant.description, "icon": "/default.png", "state": state})
+                   .success(function (data) {
+                       $location.path('/tenants');
+
+                   });
+           }
+
+            $scope.cancel = function () {
+                $location.path('/tenants');
             }
 
-            $http.post("/bonita/API/platform/tenant", {"name": tenant.name, "username": tenant.username, "password": tenant.password, "description": tenant.description, "icon": "/default.png", "state": state})
-                .success(function (data) {
-                    $location.path('/tenants');
-                });
-        }
-
-        $scope.cancel = function () {
-            $location.path('/tenants');
-        }
+       }
     });
