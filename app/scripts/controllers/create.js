9c0b0cb9d670758d943c41d204e1c33e5715563f
Colin PUY <colin.puy@bonitasoft.com>
2014-07-08 13:55:43 +0200

    fix location redirect after tenant creation

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,5 +1,5 @@
 angular.module('bonitaPlatform')
-    .controller('CreateCtrl', function ($scope, $http) {
+    .controller('CreateCtrl', function ($scope, $http, $location) {
        $scope.submit =  function(tenant) {
            var state;
            if (tenant.activated) {
@@ -10,7 +10,7 @@ angular.module('bonitaPlatform')
 
            $http.post("/bonita/API/platform/tenant", {"name":tenant.name, "username":tenant.username, "password":tenant.password,  "description":tenant.description, "icon":"/default.png", "state": state})
                .success(function(data) {
-                   $location.path('/');
+                   $location.path('/tenants');
                });
        }
     });
