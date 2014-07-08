43ce7b1000b8c7847fd28541436281aff492f271
Colin PUY <colin.puy@bonitasoft.com>
2014-07-08 13:58:09 +0200

    cancel button on creation

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,16 +1,20 @@
 angular.module('bonitaPlatform')
     .controller('CreateCtrl', function ($scope, $http, $location) {
-       $scope.submit =  function(tenant) {
-           var state;
-           if (tenant.activated) {
-               state = "ACTIVATED";
-           }else{
-               state = "DEACTIVATED";
-           }
+        $scope.submit = function (tenant) {
+            var state;
+            if (tenant.activated) {
+                state = "ACTIVATED";
+            } else {
+                state = "DEACTIVATED";
+            }
 
-           $http.post("/bonita/API/platform/tenant", {"name":tenant.name, "username":tenant.username, "password":tenant.password,  "description":tenant.description, "icon":"/default.png", "state": state})
-               .success(function(data) {
-                   $location.path('/tenants');
-               });
-       }
+            $http.post("/bonita/API/platform/tenant", {"name": tenant.name, "username": tenant.username, "password": tenant.password, "description": tenant.description, "icon": "/default.png", "state": state})
+                .success(function (data) {
+                    $location.path('/tenants');
+                });
+        }
+
+        $scope.cancel = function () {
+            $location.path('/tenants');
+        }
     });
