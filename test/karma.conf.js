5b9e818c2d9be3e2c6f4848a026c90f70becd186
Colin PUY <colin.puy@bonitasoft.com>
2014-07-08 14:40:39 +0200

    fix karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularTennisApp'));
+  beforeEach(module('bonitaPlatform'));
 
   var MainCtrl,
     scope;
