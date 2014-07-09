1e6be0ffd48866f9226f55cef3972af555034ba3
Colin PUY <colin.puy@bonitasoft.com>
2014-07-09 12:19:32 +0200

    fix edit screen api request

--- app/scripts/controllers/update.js
+++ app/scripts/controllers/update.js
@@ -2,50 +2,41 @@ angular.module('bonitaPlatform')
     .controller('UpdateCtrl', function ($scope, $http, $location, $routeParams) {
         $scope.showValidationMessages = false;
 
-        $http.get("/bonita/API/platform/tenant/"+$routeParams.tenantId)
+        $http.get("/api/tenant/" + $routeParams.tenantId)
             .success(function (data) {
                 $scope.tenant = data;
-            })
-            .error(
-                function (data) {
-                    $scope.tenant = {
-                        "defaultTenant" : true,
-                        "description" : "Default tenant",
-                        "id" : 1,
-                        "state" : "ACTIVATED"
-                    } ;
-                }
-            );
+            });
 
-        $scope.isInvalidPassword = function(pwd,conf_pwd){
+
+        $scope.isInvalidPassword = function (pwd, conf_pwd) {
 
             return (pwd != conf_pwd) && $scope.showValidationMessages;
         }
 
-        $scope.isNotValid = function(field){
+        $scope.isNotValid = function (field) {
             return $scope.showValidationMessages && field.$invalid;
         }
 
-        $scope.submit =  function(tenant) {
-           var state;
-           $scope.showValidationMessages = true;
-           $scope.form.$setDirty();
-           if($scope.form.$valid) {
-               $scope.showValidationMessages = false;
+        $scope.submit = function (tenant) {
+            var state;
+            $scope.showValidationMessages = true;
+            $scope.form.$setDirty();
+            if ($scope.form.$valid) {
+                $scope.showValidationMessages = false;
 
-               if (tenant.activated) {
-                   state = "ACTIVATED";
-               } else {
-                   state = "DEACTIVATED";
-               }
+                if (tenant.activated) {
+                    state = "ACTIVATED";
+                } else {
+                    state = "DEACTIVATED";
+                }
 
-               $http.post("/bonita/API/platform/tenant", {"name": tenant.name, "username": tenant.username, "password": tenant.password, "description": tenant.description, "icon": "/default.png", "state": state})
-                   .success(function (data) {
-                       $location.path('/tenants');
+                $http.post("/bonita/API/platform/tenant", {"name": tenant.name, "username": tenant.username, "password": tenant.password, "description": tenant.description, "icon": "/default.png", "state": state})
+                    .success(function (data) {
+                        $location.path('/tenants');
 
-                   });
-           }
-       };
+                    });
+            }
+        };
 
         $scope.cancel = function () {
             $location.path('/tenants');
